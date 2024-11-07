d6fe2a362439b6f2c990869225a96e00c37c04fe
MOSFET <kwizeraemmanuel11@gmail.com>
2024-11-07 07:19:03 +0200

    creating simple test cases

---
+++ app/products/tests/test_read_api.py
@@ -0,0 +1,6 @@
+from django.test import SimpleTestCase
+
+class TestReadAPI(SimpleTestCase):
+    def test_filter(self):
+        a = 10
+        self.assertEqual(a, 20)
--- app/products/views/read_view.py
+++ app/products/views/read_view.py
@@ -1,8 +1,22 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from ..utils.constants import ALL_PRODUCTS
 
 
 class ReadAPIView(APIView):
     def get(self, request):
-        return Response({'status': 500})
+        if not request.GET['filter']:
+            return Response({
+                'message': "No filter parameter found",
+                'status': 500
+            })
+        
+        filter = request.GET['filter']
+        
+        response = {
+            'products': ALL_PRODUCTS[filter],
+            'status': 500,
+        }
+        
+        return Response(response)
         
