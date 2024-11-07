c092e39653390613a5a3e0e8f09247b151c7f774
MOSFET <kwizeraemmanuel11@gmail.com>
2024-11-07 23:58:02 +0200

    making changes in the test case

--- app/products/tests/test_read_api.py
+++ app/products/tests/test_read_api.py
@@ -3,4 +3,4 @@ from django.test import SimpleTestCase
 class TestReadAPI(SimpleTestCase):
     def test_filter(self):
         a = 10
-        self.assertEqual(a, 20)
+        self.assertEqual(a, 50)
